ce520979f2a6a119f77a3c6fcd04e5cbb2158897
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-09-25 04:08:39 +0900

    Purple Background

--- src/Basic.js
+++ src/Basic.js
@@ -27,19 +27,23 @@ function Basic() {
 
   return (
     <div style={{ margin: "0", width: "100vw", display: "flex" }}>
-      <ProSidebar className={"side-bar"}>
-        <div className={"side-bar-inner"} style={{ width: "300px" }}>
-          <SidebarHeader className={"image"}>
-            <img src="https://github.com/JeongMyeonghoon1105/Images/blob/main/profile1.jpeg?raw=true" alt="profile" style={{ width: "100%", height: "100%" }}></img>
-          </SidebarHeader>
-          <Menu iconShape="square" style={{ margin: "0px 50px" }}>
-            <MenuItem className={"accent"} onClick={() => { setMode('About'); }}>About</MenuItem>
-            <MenuItem className={"accent"} onClick={() => { setMode('Projects'); }}>Projects</MenuItem>
-            <MenuItem className={"accent"} onClick={() => { setMode('Blog'); }}>Tech Blog</MenuItem>
-          </Menu>
-        </div>
-      </ProSidebar>
-      {content}
+      <div style={{ width: "300px" }}>
+        <ProSidebar className={"side-bar"}>
+          <div className={"side-bar-inner"} style={{ width: "300px" }}>
+            <SidebarHeader className={"image"}>
+              <img src="https://github.com/JeongMyeonghoon1105/Images/blob/main/profile1.jpeg?raw=true" alt="profile" style={{ width: "100%", height: "100%" }}></img>
+            </SidebarHeader>
+            <Menu iconShape="square" style={{ margin: "0px 50px" }}>
+              <MenuItem className={"accent"} onClick={() => { setMode('About'); }}>About</MenuItem>
+              <MenuItem className={"accent"} onClick={() => { setMode('Projects'); }}>Projects</MenuItem>
+              <MenuItem className={"accent"} onClick={() => { setMode('Blog'); }}>Tech Blog</MenuItem>
+            </Menu>
+          </div>
+        </ProSidebar>
+      </div>
+      <div style={{ width: "70vw", height: "100vh", position: "relative" }}>
+        {content}
+      </div>
     </div>
   )
 }
