ff8b94df5fb9c1d5bbd97e4c18b7f7c8207d547e
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-09-25 03:39:56 +0900

    Orange Background

--- src/About.js
+++ src/About.js
@@ -3,12 +3,18 @@ import './App.css';
 function About() {
   return (
     <div style={{ "margin-top": "30px", "margin-left": "450px" }}>
-      <p style={{ "font-weight": "bold", "font-size": "2.5rem" }}>Jeong Myeonghoon</p>
+      <p style={{ "font-weight": "bold", "font-size": "4rem" }}>
+        JEONG <text style={{ color: "rgb(189, 93, 57)" }}>MYEONGHOON</text>
+      </p>
       <p style={{ "font-weight": "bold", "font-size": "1.5rem" }}>Education</p>
+      <p style={{ "font-size": "1rem" }}>서울시립대학교 재학</p>
       <p style={{ "font-size": "1rem" }}>UNIVERSITY OF SEOUL (Undergraduate)</p>
       <br></br>
       <p style={{ "font-weight": "bold", "font-size": "1.5rem" }}>Experiences</p>
       <p style={{ "line-height": "1.5rem", "font-size": "1rem" }}>
+        동탄 코딩랩학원 강사 (2022 - )<br></br>
+        웹 개발자 (2022 - )<br></br>
+        대한민국 공군 병장 만기 전역 (2022 - )<br></br><br></br>
         Software Class Instructor (2022 - )<br></br>
         Web Developer & Manager (2022 - )<br></br>
         Korea Air Force Military Service (OSAN AB / 2020 - 2021)
--- src/Basic.js
+++ src/Basic.js
@@ -2,13 +2,14 @@ import React from "react";
 import About from './About';
 import Projects from './Projects';
 import Blog from './Blog';
+import './App.css';
 import {useState} from "react";
 
 import {
   ProSidebar,
   Menu,
   MenuItem,
-  SubMenu,
+  // SubMenu,
   SidebarHeader,
 } from "react-pro-sidebar";
 
@@ -26,55 +27,17 @@ function Basic() {
 
   return (
     <div style={{ margin: "0", width: "100vw", display: "flex" }}>
-      <ProSidebar
-        style={{
-          position: "absolute",
-          width: "400px",
-          "min-height": "100vh"
-        }}>
-        <SidebarHeader
-          style={{
-            width: "230px",
-            height: "230px",
-            margin: "85px",
-            "border-radius": "230px",
-            overflow: "hidden"
-          }}>
-          <img src="https://avatars.githubusercontent.com/u/89135740?v=4" alt="profile" style={{ width: "100%", height: "100%"}}></img>
-        </SidebarHeader>
-        <Menu iconShape="square" style={{ margin: "0px 50px" }}>
-          <SubMenu
-            title="Portfolio"
-            style={{
-              "font-size": "1.2rem"
-            }}
-            >
-            <MenuItem
-              onClick={() => {
-                setMode('About');
-              }}
-            >
-              About
-            </MenuItem>
-            <MenuItem
-              onClick={() => {
-                setMode('Projects');
-              }}
-            >
-              Projects
-            </MenuItem>
-          </SubMenu>
-          <MenuItem
-            style={{
-              "font-size": "1.2rem"
-            }}
-            onClick={() => {
-              setMode('Blog');
-            }}
-          >
-            Tech Blog
-          </MenuItem>
-        </Menu>
+      <ProSidebar className={"side-bar"}>
+        <div className={"side-bar-inner"} style={{ width: "300px" }}>
+          <SidebarHeader className={"image"}>
+            <img src="https://github.com/JeongMyeonghoon1105/Images/blob/main/profile1.jpeg?raw=true" alt="profile" style={{ width: "100%", height: "100%" }}></img>
+          </SidebarHeader>
+          <Menu iconShape="square" style={{ margin: "0px 50px" }}>
+            <MenuItem className={"accent"} onClick={() => { setMode('About'); }}>About</MenuItem>
+            <MenuItem className={"accent"} onClick={() => { setMode('Projects'); }}>Projects</MenuItem>
+            <MenuItem className={"accent"} onClick={() => { setMode('Blog'); }}>Tech Blog</MenuItem>
+          </Menu>
+        </div>
       </ProSidebar>
       {content}
     </div>
