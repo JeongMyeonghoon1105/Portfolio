3ff1846004ad49afefdc2ce057619d6406bd23dc
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-09-25 04:24:54 +0900

    Back To Orange

--- src/About.js
+++ src/About.js
@@ -4,12 +4,12 @@ function About() {
   return (
     <div style={{ position: "absolute", top: "0", bottom: "0", margin: "auto 70px", height: "700px" }}>
       <p style={{ "font-weight": "bold", "font-size": "4rem" }}>
-        JEONG <text style={{ color: "rgb(100, 30, 120)" }}>MYEONGHOON</text>
+        JEONG <text style={{ color: "rgb(189, 93, 57)" }}>MYEONGHOON</text>
       </p>
       <p style={{ "font-weight": "bold", "font-size": "1.5rem" }}>Education</p>
       <p style={{ "font-size": "1rem", lineHeight: "1.8rem" }}>
         서울시립대학교 정경대학 재학<br></br>
-        서원고등학교 (14회)
+        서원고등학교 졸업 (14회)
       </p>
       <br></br>
       <p style={{ "font-weight": "bold", "font-size": "1.5rem" }}>Experiences</p>
