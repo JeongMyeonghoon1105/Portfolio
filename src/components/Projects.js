bdf6225d053e78872c0b5e6a737e321b7c2a1bb4
cobit-git <76054530+cobit-git@users.noreply.github.com>
2022-09-26 17:41:18 +0900

    Update Projects Tab Contents

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,14 +8,18 @@ function Projects() {
       </p>
       <div>
         <p>1. 코딩랩학원 웹사이트</p>
-        <img src = "https://github.com/JeongMyeonghoon1105/Images/blob/main/CodingLab.png?raw=true" alt="" style={{ width: "500px" }}></img>
-        <p>URL</p>
-        <a>http://동탄코딩랩.com</a><br></br>
-        <p>사용 기술</p>
-        <p>Frontend</p>
-        <p>Backend</p>
-        <p>상세 정보</p>
-        <a>https://github.com/JeongMyeonghoon1105/CodingLab</a><br></br>
+        <img src = "https://github.com/JeongMyeonghoon1105/Images/blob/main/CodingLab.png?raw=true" alt="" style={{ width: "1000px" }}></img>
+        <p style={{ "font-weight": "bold", "font-size": "1.5rem" }}>URL</p>
+        <a href='http://동탄코딩랩.com'>http://동탄코딩랩.com</a><br></br>
+        <p style={{ "font-weight": "bold", "font-size": "1.5rem" }}>사용 기술</p>
+        <p style={{ "font-weight": "bold", "font-size": "1.2rem" }}>Frontend</p>
+        jQuery
+        <p style={{ "font-weight": "bold", "font-size": "1.2rem" }}>Backend</p>
+        Node.js express
+        MySQL
+        Ubuntu Server
+        <p style={{ "font-weight": "bold", "font-size": "1.5rem" }}>상세 정보</p>
+        <a href='https://github.com/JeongMyeonghoon1105/CodingLab'>https://github.com/JeongMyeonghoon1105/CodingLab</a><br></br>
       </div>
     </div>
   );
