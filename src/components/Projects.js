29d69a634f41b001f8443feb858179a95e185676
cobit-git <76054530+cobit-git@users.noreply.github.com>
2022-09-26 17:44:55 +0900

    Overflow

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,12 +2,12 @@ import '../styles/App.css';
 
 function Projects() {
   return (
-    <div style={{ margin: "100px 70px", width: "100%", "min-height": "700px" }}>
+    <div style={{ margin: "100px 70px", width: "100%", "min-height": "700px", overflow: "scroll" }}>
       <p style={{ "font-weight": "bold", "font-size": "4rem" }}>
         Projects
       </p>
       <div>
-        <p>1. 코딩랩학원 웹사이트</p>
+        <p style={{ "font-weight": "bold", "font-size": "2rem" }}>1. 코딩랩학원 웹사이트</p>
         <img src = "https://github.com/JeongMyeonghoon1105/Images/blob/main/CodingLab.png?raw=true" alt="" style={{ width: "1000px" }}></img>
         <p style={{ "font-weight": "bold", "font-size": "1.5rem" }}>URL</p>
         <a href='http://동탄코딩랩.com'>http://동탄코딩랩.com</a><br></br>
