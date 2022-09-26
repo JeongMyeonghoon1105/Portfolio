e352423e92df7312f4c00eec675011579096094d
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-09-27 00:28:30 +0900

    Blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,11 @@
+import "../styles/Blog.css";
+
 function Blog() {
   return (
-    <div></div>
+    <div className={ "blog-body" }>
+      <p className={ "blog-title" }>Tech Blog</p>
+      <p className={ "blog-subtitle" }>개발 진행중입니다.</p>
+    </div>
   );
 }
 
