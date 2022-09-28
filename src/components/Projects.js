48c449508803e3f47cfc8f8f74644d364cec12d6
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-09-28 22:30:50 +0900

    Scroll

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,34 +20,6 @@ function Projects() {
           <li>MySQL</li>
           <li>Ubuntu Server</li>
         </ul>
-        <Individual
-          title="1. 코딩랩학원 웹사이트"
-          url="http://동탄코딩랩.com"
-          github="https://github.com/JeongMyeonghoon1105/CodingLab"
-          img="https://github.com/JeongMyeonghoon1105/Images/blob/main/CodingLab.png?raw=true"
-        />
-        <p className={ "proj-subtitle" }>Frontend</p>
-        <ul className={ "proj-content" }><li>jQuery</li></ul>
-        <p className={ "proj-subtitle" }>Backend</p>
-        <ul className={ "proj-content" }>
-          <li>Node.js express</li>
-          <li>MySQL</li>
-          <li>Ubuntu Server</li>
-        </ul>
-        <Individual
-          title="1. 코딩랩학원 웹사이트"
-          url="http://동탄코딩랩.com"
-          github="https://github.com/JeongMyeonghoon1105/CodingLab"
-          img="https://github.com/JeongMyeonghoon1105/Images/blob/main/CodingLab.png?raw=true"
-        />
-        <p className={ "proj-subtitle" }>Frontend</p>
-        <ul className={ "proj-content" }><li>jQuery</li></ul>
-        <p className={ "proj-subtitle" }>Backend</p>
-        <ul className={ "proj-content" }>
-          <li>Node.js express</li>
-          <li>MySQL</li>
-          <li>Ubuntu Server</li>
-        </ul>
       </div>
     </div>
   );
