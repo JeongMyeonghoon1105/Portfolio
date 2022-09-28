0e1d6726f052c7aa787073bc6274d8bbd29da9f2
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-09-28 23:36:30 +0900

    Active

--- src/components/Basic.js
+++ src/components/Basic.js
@@ -3,6 +3,7 @@ import '../styles/Basic.css';
 import About from './About';
 import Projects from './Projects';
 import Blog from './Blog';
+import Experience from './Experience';
 // import {useState} from "react";
 
 import {
@@ -18,18 +19,27 @@ function Basic() {
 
   document.addEventListener('scroll', (event) => {
     let about = document.getElementById("about-container").scrollHeight;
-    let projects = document.getElementById("projects-container").scrollHeight;
+    let experience = document.getElementById("experience-container").scrollHeight + about;
+    let projects = document.getElementById("projects-container").scrollHeight + experience;
     
     if (window.scrollY > projects) {
       document.getElementById("about-tab").style.color="white";
+      document.getElementById("experience-tab").style.color="white";
       document.getElementById("projects-tab").style.color="white";
       document.getElementById("blog-tab").style.color="gray";
+    } else if (window.scrollY > experience) {
+      document.getElementById("about-tab").style.color="white";
+      document.getElementById("experience-tab").style.color="white";
+      document.getElementById("projects-tab").style.color="gray";
+      document.getElementById("blog-tab").style.color="white";
     } else if (window.scrollY > about) {
       document.getElementById("about-tab").style.color="white";
-      document.getElementById("projects-tab").style.color="gray";
+      document.getElementById("experience-tab").style.color="gray";
+      document.getElementById("projects-tab").style.color="white";
       document.getElementById("blog-tab").style.color="white";
     } else {
       document.getElementById("about-tab").style.color="gray";
+      document.getElementById("experience-tab").style.color="white";
       document.getElementById("projects-tab").style.color="white";
       document.getElementById("blog-tab").style.color="white";
     }
@@ -44,9 +54,10 @@ function Basic() {
               <img src={src} alt="profile" className={ "profile" }></img>
             </SidebarHeader>
             <Menu iconShape="square" style={{ margin: "0px 50px" }}>
-              <div className={"accent"} id={"about-tab"} onClick={() => { document.getElementById("about-top").scrollIntoView({behavior: "smooth"}) }}>About</div>
-              <div className={"accent"} id={"projects-tab"} onClick={() => { document.getElementById("projects-top").scrollIntoView({behavior: "smooth"}) }}>Projects</div>
-              <div className={"accent"} id={"blog-tab"} onClick={() => { document.getElementById("blog-container").scrollIntoView({behavior: "smooth"}) }}>Tech Blog</div>
+              <div className={"accent"} id={"about-tab"} onClick={() => { document.getElementById("about-top").scrollIntoView({behavior: "smooth"}) }}>ABOUT</div>
+              <div className={"accent"} id={"experience-tab"} onClick={() => { document.getElementById("experience-top").scrollIntoView({behavior: "smooth"}) }}>DETAILS</div>
+              <div className={"accent"} id={"projects-tab"} onClick={() => { document.getElementById("projects-top").scrollIntoView({behavior: "smooth"}) }}>PROJECTS</div>
+              <div className={"accent"} id={"blog-tab"} onClick={() => { document.getElementById("blog-container").scrollIntoView({behavior: "smooth"}) }}>TECH BLOG</div>
             </Menu>
           </div>
         </ProSidebar>
@@ -55,6 +66,8 @@ function Basic() {
         <div style={{ width: "70vw", height: "100vh" }}>
           <About/>
         </div>
+        <div style={{ width: "100%", height: "0px", border: "none" }} id={ "experience-top" }></div>
+        <Experience/>
         <div style={{ width: "100%", height: "0px", border: "none" }} id={ "projects-top" }></div>
         <Projects/>
         <Blog/>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,23 +1,18 @@
-import '../styles/About.css'
+import "../styles/Experience.css"
 
 function Experience() {
   return (
-    <div className={ "about-body" } id={"about-container"}>
-      <p className={ "about-subtitle" }>Education</p>
-      <p className={ "about-content" }>
-        서울시립대학교 정경대학 재학<br></br>
-        서원고등학교 졸업 (14회)
-      </p>
-      <br></br>
-      <p className={ "about-subtitle" }>Experiences</p>
-      <p className={ "about-content" }>
-        동탄 코딩랩학원 강사 (2022 - )<br></br>
-      </p>
-      <br></br>
-      <p className={ "about-subtitle" }>Contact</p>
-      <p className={ "about-content" }>
-        wjdaudgns1105@gmail.com
-      </p>
+    <div className={ "experience-body" } id={ "experience-container" }>
+      <p className={ "experience-title" }>Education</p>
+      <div>
+        <p className={ "experience-subtitle" }>서울시립대학교 정경대학 재학</p>
+        <p className={ "experience-subtitle" }>서원고등학교 졸업 (14회)</p>
+      </div>
+      <p className={ "experience-title" }>Experience</p>
+      <div>
+        <p className={ "experience-subtitle" }>동탄 코딩랩학원 강사 (2022.03 - )</p>
+        <p className={ "experience-subtitle" }>대한민국 공군 항공통제병 근무 (2020 - 2021)</p>
+      </div>
     </div>
   );
 }
