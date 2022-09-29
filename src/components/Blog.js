473b0c86434965332d82762f8695d918ac8f313e
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2022-09-29 20:50:35 +0900

    Upper Case

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,7 +4,7 @@ function Blog() {
   return (
     <div className={ "blog-body" } id={ "blog-container" }>
       <div className={ "blog-content" }>
-        <p className={ "blog-title" }>Tech Blog</p>
+        <p className={ "blog-title" }>TECH BLOG</p>
         <p className={ "blog-subtitle" }>개발 진행중입니다.</p>
       </div>
     </div>
